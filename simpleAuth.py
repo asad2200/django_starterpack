6416fd6f3e770be5d5ed985901c22b9486b00a55
asad2200 <asadjakhavala92@gmail.com>
2021-06-28 20:37:22 +0530

    Project finished

---
+++ simpleAuth.py
@@ -0,0 +1,5 @@
+DOMAIN = "http://127.0.0.1:8000"  # for default djangoserver (use your domain name)
+SENDER_EMAIL_ID = "YOUR_MAIL_ID"
+SENDER_PASSWORD = "YOUR MAIL_ID PASSWORD"
+SERVER_NAME = "smtp.gmail.com"  # for gmail (use your mail servername)
+# NOTE : TURN ON LESS SECURE APP OPTION IN UR GOOGLE ACCOUNT
